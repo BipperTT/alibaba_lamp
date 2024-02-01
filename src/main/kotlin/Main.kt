9566fa93b019d4699da6c38664c4da1aabd31ce3
Alexis <alexis.checa.7e7@itb.cat>
2024-02-01 22:04:21 +0100

    3er Commit: completar main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,23 @@
 package org.example
 
 fun main() {
+    val lampara1 = Lampara()
+    lampara1.set_identificador("Lámpara del comedor")
 
+    val lampara2 = Lampara()
+    lampara2.set_identificador("Lámpara de la cocina")
+
+    lampara1.encender()
+    repeat(3) { lampara1.cambiar_color() }
+    lampara1.cambiar_intensidad()
+
+
+    lampara2.encender()
+    repeat(2) { lampara2.cambiar_color() }
+    lampara2.cambiar_intensidad()
+    lampara2.apagar()
+    lampara2.cambiar_color()
+    lampara2.encender()
+    lampara2.cambiar_color()
+    lampara2.cambiar_intensidad()
 }
